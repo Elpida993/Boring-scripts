8abc2edebb31a95a8262819f787837180699315a
Joseph Ferruzza <45747644+JosephFerruzza@users.noreply.github.com>
2020-09-14 20:29:44 -0700

    Add files via upload
    Nothing special here. Its just something I will find useful over time and don't want to forget it

---
+++ 5times-loop.py
@@ -0,0 +1,11 @@
+import time
+
+counter = 0
+n = 0
+while n >= 0:
+    for i in range(5):
+        counter += 1
+        print(counter, "\n", end='')
+        #time.sleep(0.05)
+    print()
+    #time.sleep(1)
